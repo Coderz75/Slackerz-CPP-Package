c67d8e31f0e067c127ecc30a670f575c91c2ceb1
Nuaym Syed <scienceboy75@gmail.com>
2022-02-03 18:16:34 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,5 @@ using namespace std;
 
 
 int main() {
-  prints("Hello");
-  printn(50.3434);
-  printn(fint(3544.2432));
-  printn(sint("1"));
-  prints(nums(1));
-  string bob = input("Type your name: ");
-  prints("Hello " + bob);
+  //Enter code here with
 }
