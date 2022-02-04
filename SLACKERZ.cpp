766cdf35d69c132baf33fe9675e648089191b908
Nuaym Syed <scienceboy75@gmail.com>
2022-02-04 02:15:12 +0000

    Alot, got configuration ready

---
+++ SLACKERZ.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+using namespace std;
+// print
+void prints(std::string print){
+  std::cout << print + "\n";
+}
+void printn(float number){
+  cout << to_string(number) + "\n";
+}
+
+//CONVERSIONS
+//To int
+
+int fint(float number){
+  return (int)number;
+}
+int sint(std::string something){
+  return stoi(something);
+}
+
+//to string
+
+string nums(float num){
+  return to_string(num);
+}
+
+string input(std::string ask){
+  string x;
+  cout << ask;
+  cin >> x;
+  return x;
+}
---
+++ runner.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void prints(std::string);
+void printn(float number);
+int fint(float number);
+
+int sint(std::string something);
+
+//to string
+
+std::string nums(float num);
+
+std::string input(std::string ask);
