4f0a5759be47f7b1c23043bc3ff09c96aa2a21d4
Nuaym Syed <scienceboy75@gmail.com>
2022-02-06 19:54:09 +0000

    Source

--- SLACKERZ.cpp
+++ SLACKERZ.cpp
@@ -1,5 +1,5 @@
 //Versinon: Adaptive
-//Source: https://github.com/Coderz75/Slackerz-Compiler
+//Source: https://github.com/Coderz75/Slackerz-Compiler/tree/test
 //This code will not automatically update, you must manually re-download the files.
 //Branch: test [unstable]
 #include <iostream>
