71114b93036bd5c182fa5ff1f7edb075af085cd5
Nuaym Syed <scienceboy75@gmail.com>
2022-02-06 11:55:20 -0800

    Update SLACKERZ.cpp

--- SLACKERZ.cpp
+++ SLACKERZ.cpp
@@ -1,16 +1,27 @@
-//Versinon: 1.0
-//Source: https://github.com/Coderz75/Slackerz-Compiler
-//This code will not automatically update, you must manually re-download the files. 
 
+//Versinon: 2.0
+//Source: https://github.com/Coderz75/Slackerz-Compiler/
+//This code will not automatically update, you must manually re-download the files.
+//Branch: main [stable]
 #include <iostream>
+#include <sstream>
 #include<string>
+#include <list>
+#include <algorithm>
+#include <vector>
 using namespace std;
+
 // print
 void prints(std::string print){
   std::cout << print + "\n";
 }
 void printn(float number){
   cout << to_string(number) + "\n";
+}
+
+void printv(std::vector<string> a){
+    for(int i=0; i < a.size(); i++)
+    std::cout << a.at(i) << '\n';
 }
 
 //CONVERSIONS
@@ -34,4 +45,22 @@ string input(std::string ask){
   cout << ask;
   cin >> x;
   return x;
+}
+
+std::vector<std::string>  split(string a, string x){
+    std::vector<std::string> so {};
+    string s = a;
+    string delim = x;
+    auto start = 0U;
+    auto end = s.find(delim);
+    while (end != std::string::npos)
+    {
+        string m = s.substr(start, end - start);
+        so.push_back(m);
+        start = end + delim.length();
+        end = s.find(delim, start);
+    }
+
+    so.push_back(s.substr(start, end));
+    return so;
 }
