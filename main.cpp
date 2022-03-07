0eb99c32a134f7ddcde3c0c690fd06ca1854222c
Nuaym Syed <scienceboy75@gmail.com>
2022-03-07 03:16:49 +0000

    cleaned up all tests

--- main.cpp
+++ main.cpp
@@ -8,32 +8,6 @@ using namespace std;
 
 int main() {
     init_slackerz();
-    //tests prints and printn
-    //print("Hello");
-    //print(50.3434);
-    //tests conversions
-    //printn(fint(3544.2432));
-    //printn(sint("1"));
-    //print(nums(1));
-    //tests input
-    //string bob = input("Type your name: ");
-    //print("Hello " + bob);
-    //tests split and vector
-    //std::vector<string> a = split("a=b=c", "=");
-    //printv(a);
-    //printv(range(5));
-    //string bom = input("COMO ESTAS: ");
-    //print(bom);
-
-    //more tests
-    
-    //string como = input("HOLA COMO ESTAS ");
-    //print(como);
-    //if(como == "SOY DORA"){
-      //print("NOOOOOOOOOOo");
-      //print("oogley");
-    //}
-    //printn(random(34));
 
     print("EVERYTHING WORKS FINE");
 
