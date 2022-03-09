bcd2202f6295d1a0f8d5d36cd4545eb9256097b1
Nuaym Syed <scienceboy75@gmail.com>
2022-03-09 23:54:06 +0000

    fixes some stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,4 @@
-#include <iostream> //includes default c++
-#include "slackerz/default.h" //includes default package of slackerz (you can do dev for dev package, imports for import package, and all for everything, but default should be enough)
-#include<string> //not required, but reccomended 
-using namespace std; //not required, but simplifies a whole lot
+#include "slackerz/default.h" //includes default package of slackerz (you can do dev for dev package, imports for import package, and all for everything, but default should be enough, time for time package)
 
 
 
