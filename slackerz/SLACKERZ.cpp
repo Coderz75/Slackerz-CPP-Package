f61ac0e0f46f48100f538fb3b1839eb5a586fb49
Nuaym Syed <scienceboy75@gmail.com>
2022-03-09 16:04:10 -0800

    Update SLACKERZ.cpp

--- slackerz/SLACKERZ.cpp
+++ slackerz/SLACKERZ.cpp
@@ -1,7 +1,3 @@
-//Version: beta 5
-//Source: https://github.com/Coderz75/Slackerz-Compiler/tree/test
-//This code will not automatically update, you must manually re-download the files.
-//Branch: test [unstable]
 #include <iostream>
 #include<string>
 #include <algorithm>
