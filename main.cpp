8981cfbd3bcff8811ed9c798696ac1dcc3f5b4a5
Nuaym Syed <scienceboy75@gmail.com>
2022-03-17 18:06:14 -0700

    again

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include "slackerz/default.h"
-#include "slackerz/imports.h"
 using namespace slackerz;
 
 int main() {
     init_slackerz();
     print("Welcome to SLACKERZ!");
     print("cool right?");
-
 }
