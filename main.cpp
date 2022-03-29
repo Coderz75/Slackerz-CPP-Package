028afee6220f9b713f0371b12122851282f03e63
Nuaym Syed <scienceboy75@gmail.com>
2022-03-28 17:50:25 -0700

    Reset main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,4 @@ int main() {
     init_slackerz();
     print("Welcome to SLACKERZ!");
     print("Cool right?");
-    slist a({"hola","como","estas"});
-    slist b({"hola"});
-    a.extend(b);
-    print(a.v);
 }
