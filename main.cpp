223f6c4028bcc71687db55a537b81f49b90dd463
Coderz75 <scienceboy75@gmail.com>
2022-09-18 11:06:49 -0700

    Verson 1.2 ready for launch

--- main.cpp
+++ main.cpp
@@ -2,30 +2,5 @@
 
 int main() {
     init_slackerz();
-    print("Welcome to Slackerz!","yeah", 483);
-    vector<string> a = {"yeah","imagine"};
-    s::list<string> m  = {"Lol","cool"};
-	m.append("right");
-    m.insert(1,"xd");
-    m.extend(a);
-    s::list<string> k = {"wow"};
-    m.extend(k);
-    m.pop();
-    m.pop(0);
-    m.append("xd");
-    m.remove("xd");
-    m.clear();
-    m = {"Welp","got deleted"};
-    m.extend("Lol","cool");
-	print(m);
-    s::list<string> z = m.copy();
-    m.extend(z);
-    print(m);
-    print(m.count("Welp"));
-    print(m.index("Welp"),m.index("Lol"));
-    s::list<string> l = {"Lol","cool","yeah","imagine"};
-    print(l);
-    l.reverse();
-    print(l);
-}   
+    print("Welcome to Slackerz!");}   
 
